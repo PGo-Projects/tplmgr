5634f16c1722df6ca3133b97f500d0666de90b7c
Patrick Chan <patrickchan1999@gmail.com>
2018-09-08 08:09:19 -0400

    Add function for rendering template given ResponseWriter, name, and data

---
+++ renderer.go
@@ -0,0 +1,25 @@
+package tplmgr
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Render(w http.ResponseWriter, name string, data interface{}) {
+	template, ok := templates[name]
+	if !ok {
+		http.Error(w, fmt.Sprintf("The template %s does not exist.", name),
+			http.StatusInternalServerError)
+	}
+
+	buf := bufpool.Get()
+	defer bufpool.Put(buf)
+
+	err := template.Execute(buf, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	buf.WriteTo(w)
+}
